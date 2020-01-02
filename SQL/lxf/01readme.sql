681f0115f1e3376f70c86cd05ad2a177228ddcc8
zhihao.zheng <zhengzhihao994@outlook.com>
2020-01-02 19:01:57 +0800

    Add the SQL notes

---
+++ SQL/lxf/01readme.sql
@@ -0,0 +1,12 @@
+-- 现代程序离不开关系数据库，要使用关系数据库就要掌握 SQL
+
+-- NoSQL
+-- 非 SQL的数据库，包括 MongoDB、Cassandra、Dynamo等
+-- NoSQL发展历程
+-- 1970: NoSQL = We have no SQL
+-- 1980: NoSQL = Know SQL
+-- 2000: NoSQL = No SQL!
+-- 2005: NoSQL = Not only SQL
+-- 2013: NoSQL = No, SQL!
+
+-- SQL数据库仍然承担了各种应用程序的核心数据存储，而 NoSQL数据库作为 SQL数据库的补充，不是二选一关系，是主从关系
---
+++ SQL/lxf/03install_mysql.sql
@@ -0,0 +1,18 @@
+-- MySQL的开源衍生版本
+--  MariaDB
+--  Aurora
+--  PolarDB
+
+-- MySQL的官方版本
+--  Community Edition 社区开源版本，免费
+--  Standard Edition 标准版
+--  Enterprise Edition 企业版
+--  Cluster Carrier Grade Edition 集群版
+
+-- 安装 MySQL
+
+-- 运行 MySQL
+--  mysql -u root -p
+--  输入口令
+--  连接到 MySQL服务器
+--  exit退出 MySQL命令行，MySQL服务器仍在后台运行
